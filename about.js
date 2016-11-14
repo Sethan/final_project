b03ef21e027fedc9c11f5ea4b7099600fa713404
siham-sidali <sihamsidali@hotmail.com>
2016-11-14 11:13:09 +0100

    Create about.js

---
+++ about.js
@@ -0,0 +1,15 @@
+var myimage=document.getElementById("bilder");
+var imageArray=["img/siham.jpg","img/lars.jpg","img/idriss.jpg","img/demo_4.png"];
+var index=0;
+function changeimage(){
+  bilder.setAttribute("src" , imageArray[index]);
+  index++;
+  if(index>= imageArray.length){
+    index=0;
+
+  }
+}
+var inter=setInterval(changeimage, 3000);
+bilder.onclick=function(){
+  clearInterval(inter);
+}
