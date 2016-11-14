72adf7453ad26f28059e5d108d9e31f833aa8f98
siham-sidali <sihamsidali@hotmail.com>
2016-11-14 13:33:34 +0100

    Update about.js

--- about.js
+++ about.js
@@ -3,7 +3,7 @@ var myimage=document.getElementById("bilder");
 var imageArray=["img/siham.jpg","img/lars.jpg","img/idriss.jpg","img/Portrett.jpg"];
 var index=0;
 function changeimage(){
-  bilder.setAttribute("src" , imageArray[index]);
+  bilder.setAttribute("class" , imageArray[index]);
   index++;
   if(index>= imageArray.length){
     index=0;
